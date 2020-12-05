cc2db2066a08e2640926fdb8dc2227b6d6b5700f
Vinay-murahari <70923761+Vinay-murahari@users.noreply.github.com>
2020-12-06 10:51:06 +1100

    Add files via upload

---
+++ student_test-1/student.sql
@@ -0,0 +1,4 @@
+insert into CAL.student(student_id,student_adress,student_fname,student_lname)
+values(101,"Burwood","vinay","murahari"),
+(102,"Burwood","vaishali","thakoor"),
+(103,"Boxhill","Raju","Murahari");
